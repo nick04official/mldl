8733719f9d58287d045840ff5c64a5fc1ace0dc6
Francesco Bianco Morghet <36233478+francibm97@users.noreply.github.com>
2020-05-15 10:44:49 +0200

    Integrated Ego-RNN two stream

---
+++ models/two_stream_model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+from models.resnet.flow_resnet import flow_resnet34
+from attention_model import AttentionModel
+
+class twoStreamAttentionModel(nn.Module):
+    def __init__(self, flowModel='', frameModel='', stackSize=5, memSize=512, num_classes=61):
+        super(twoStreamAttentionModel, self).__init__()
+
+        self.flowModel = flow_resnet34(False, channels=2*stackSize, num_classes=num_classes)
+        if flowModel != '':
+            self.flowModel.load_state_dict(torch.load(flowModel))
+        
+        self.frameModel = AttentionModel(num_classes, memSize)
+        if frameModel != '':
+            self.frameModel.load_state_dict(torch.load(frameModel))
+        
+        self.fc2 = nn.Linear(512 * 2, num_classes, bias=True)
+        self.dropout = nn.Dropout(0.5)
+        self.classifier = nn.Sequential(self.dropout, self.fc2)
+
+    def forward(self, inputVariableFlow, inputVariableFrame):
+        _, flowFeats = self.flowModel(inputVariableFlow)
+        _, rgbFeats = self.frameModel(inputVariableFrame)
+        twoStreamFeats = torch.cat((flowFeats, rgbFeats), 1)
+        return self.classifier(twoStreamFeats)
